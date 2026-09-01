20651c1ffcadb9d139a05e749fb47d22095784b4
justvihxn <justvihxn@gmail.com>
2026-09-01 19:49:22 +0530

    Update paid.py

--- paid.py
+++ paid.py
@@ -6151,7 +6151,7 @@ async def deploy_cmd(ctx):
 
     # Check if deploy is configured
     if DEPLOY_ROLE_ID == 0:
-        await ctx.send(embed=create_error_embed("100% Free VPS are turned Off ❌", "Join https://dsc.gg/justvihxn and invite 2 people or pay 5 inr for a free vps right now"))
+at         await ctx.send(embed=create_error_embed("100% Free VPS are turned Off ❌", "Check Out Plans at https://discord.com/channels/1540619481494200371/1540619484786855959 , If You Haven't Joined The Server Join at https://dsc.gg/justvihxn"))
         return
 
     # Check role
